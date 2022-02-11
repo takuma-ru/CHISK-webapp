a3b8239c7ec8412d5b996414b76d4bb52f8ba8a6
takuma-ru <kcattdev@gmail.com>
2022-02-12 01:45:31 +0900

    [Add] firebase, icon image

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,46 +57,39 @@ export default {
       theme_color: '#7786FF',
       icons: [
         {
-          src: '/icon.png',
+          src: '/icon_64.png',
           sizes: '64x64',
           type: 'image/png',
-          purpose: 'any maskable',
         },
         {
-          src: '/icon.png',
+          src: '/icon_120.png',
           sizes: '120x120',
           type: 'image/png',
-          purpose: 'any maskable',
         },
         {
-          src: '/icon.png',
+          src: '/icon_144.png',
           sizes: '144x144',
           type: 'image/png',
-          purpose: 'any maskable',
         },
         {
-          src: '/icon.png',
+          src: '/icon_152.png',
           sizes: '152x152',
           type: 'image/png',
-          purpose: 'any maskable',
         },
         {
-          src: '/icon.png',
+          src: '/icon_192.png',
           sizes: '192x192',
           type: 'image/png',
-          purpose: 'any maskable',
         },
         {
-          src: '/icon.png',
+          src: '/icon_384.png',
           sizes: '384x384',
           type: 'image/png',
-          purpose: 'any maskable',
         },
         {
-          src: '/icon.png',
+          src: '/icon_512.png',
           sizes: '512x512',
           type: 'image/png',
-          purpose: 'any maskable',
         },
       ],
       screenshots: [
