0775ff2db5f5a52d41edb8d5706cf4f0df010558
takuma-ru <kcattdev@gmail.com>
2022-02-13 15:42:18 +0900

    [Fix] sign in Redirect

--- composable/firebase/auth.ts
+++ composable/firebase/auth.ts
@@ -48,7 +48,7 @@ export default function auth () {
   const trySignIn = () => {
     setPersistence(auth, browserLocalPersistence)
       .then(() => {
-        signInWithPopup(auth, provider).then((result) => {
+        return signInWithPopup(auth, provider).then((result) => {
           const credential = GoogleAuthProvider.credentialFromResult(result)
           const token = credential?.accessToken
           const user = result.user
