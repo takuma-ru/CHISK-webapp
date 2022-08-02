34266ec3cddea3aa8ffacf00a86abcd67a4e4c1d
takuma-ru <kcattdev@gmail.com>
2022-08-02 13:23:21 +0900

    [Add] @takuma-ru/vue-swipe-modal

--- composable/utils/returnMdiIcon.ts
+++ composable/utils/returnMdiIcon.ts
@@ -1,7 +1,8 @@
 /**
-  引数で指定されたmdiアイコンを返す
-**/
-
+ * 引数で指定されたmdiアイコンを返す
+ * @param icon string
+ * @returns string
+ */
 export default function returnMdiIcon (icon: string) {
   const mdiIcon = require('@mdi/js')
 
